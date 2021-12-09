2bd759e285b6ecfc31b55469ec74f1a1df213147
Chris Cassano <chris@litprotocol.com>
2021-12-09 16:34:04 -0500

    lots of improvements to staking

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -18,27 +19,35 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
 
     IERC20 public stakingToken;
 
-    mapping(address => uint256) public userRewardPerTokenPaid;
+    // epoch vars
+    uint256 public epoch;
+    uint256 public lastEpochBlock;
+    uint256 public blocksBetweenEpochs;
+
+    uint256 public tokenRewardPerTokenPerEpoch;
     mapping(address => uint256) public rewards;
 
-    uint256 private _totalSupply;
-    mapping(address => uint256) private _balances;
+    uint256 public totalStaked;
+    mapping(address => uint256) private balances;
+    address[] public joiners;
+    address[] public leavers;
+    address[] public validators;
 
-    mapping(address => uint32) ips;
+
+    mapping(address => uint32) public ips;
+    mapping(address => uint32) public ports;
 
     /* ========== CONSTRUCTOR ========== */
     constructor(address _stakingToken) {
         stakingToken = IERC20(_stakingToken);
+        blocksBetweenEpochs = 1;
+        tokenRewardPerTokenPerEpoch = 10^ERC20(address(stakingToken)).decimals();
     }
 
     /* ========== VIEWS ========== */
 
-    function totalSupply() external view returns (uint256) {
-        return _totalSupply;
-    }
-
     function balanceOf(address account) external view returns (uint256) {
-        return _balances[account];
+        return balances[account];
     }
 
     function getIp(address addr) external view returns (uint32) {
@@ -47,25 +56,88 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
-    function stake(uint256 amount, uint32 ip)
-        external
+    function advanceEpoch() public {
+        require(block.number > lastEpochBlock + blocksBetweenEpochs, "Enough blocks have not elapsed since the last epoch");
+        // add the joiners
+        for(uint i = 0; i < joiners.length; i++){
+            validators.push(joiners[i]);
+        }
+        delete joiners;
+
+        // remove the leavers
+        bool contained;
+        uint256 index;
+        for(uint i = 0; i < leavers.length; i++){
+            (contained, index) = arrayContains(validators, leavers[i]);
+            if (contained) {
+                removeFromArray(validators, index);
+            }
+        }
+        delete leavers;
+
+        epoch++;
+        lastEpochBlock = block.number;
+    }
+
+    function setBlocksBetweenEpochs(uint256 newBlocksBetweenEpochs) public onlyOwner {
+        blocksBetweenEpochs = newBlocksBetweenEpochs;
+    }
+
+
+    function stakeAndJoin(uint256 amount, uint32 ip, uint32 port)
+        public
         nonReentrant
         whenNotPaused
     {
         require(amount > 0, "Cannot stake 0");
-        _totalSupply = _totalSupply.add(amount);
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        totalStaked = totalStaked.add(amount);
+
+        // check if they are already a validator
+        bool contained;
+        uint256 index;
+        (contained, index) = arrayContains(validators, msg.sender);
+
+        if (!contained && balances[msg.sender] == 0) { 
+            // add to joiners
+            joiners.push(msg.sender);
+        }
+
+        balances[msg.sender] = balances[msg.sender].add(amount);
         stakingToken.safeTransferFrom(msg.sender, address(this), amount);
         ips[msg.sender] = ip;
+        ports[msg.sender] = port;
         emit Staked(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) public nonReentrant {
         require(amount > 0, "Cannot withdraw 0");
-        _totalSupply = _totalSupply.sub(amount);
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        bool contained;
+        uint256 index;
+        (contained, index) = arrayContains(validators, msg.sender);
+        require(contained == false, "Active validators cannot leave.  Please use the leave() function and wait for the next epoch to leave");
+        totalStaked = totalStaked.sub(amount);
+        balances[msg.sender] = balances[msg.sender].sub(amount);
         stakingToken.safeTransfer(msg.sender, amount);
         emit Withdrawn(msg.sender, amount);
+    }
+
+    function requestToLeave() public{
+        bool contained;
+        uint256 index;
+        (contained, index) = arrayContains(leavers, msg.sender);
+        require(contained == false, "You have already signaled that you wish to leave");
+
+        // handle the case where the user decides to leave before they become a validator
+        (contained, index) = arrayContains(joiners, msg.sender);
+        if (contained) {
+            removeFromArray(joiners, index);
+            return;
+        }
+
+        (contained, index) = arrayContains(validators, msg.sender);
+        require(contained == true, "You must be either an active validator or on the list of joiners to request to leave");
+
+        leavers.push(msg.sender);
     }
 
     function getReward() public nonReentrant {
@@ -78,12 +150,36 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
     }
 
     function exit() external {
-        withdraw(_balances[msg.sender]);
+        withdraw(balances[msg.sender]);
         getReward();
     }
 
-    function setIp(uint32 ip) external nonReentrant {
+    function setIpAndPort(uint32 ip, uint32 port) external nonReentrant {
         ips[msg.sender] = ip;
+        ports[msg.sender] = port;
+    }
+
+    function setStakingToken(address newStakingTokenAddress) public onlyOwner {
+        stakingToken = IERC20(newStakingTokenAddress);
+    }
+
+    function setTokenRewardPerTokenPerEpoch(uint256 newTokenRewardPerTokenPerEpoch) public onlyOwner {
+        tokenRewardPerTokenPerEpoch = newTokenRewardPerTokenPerEpoch;
+    }
+
+    function arrayContains(address[] storage array, address toFind) internal view returns (bool, uint256) {
+        for(uint i = 0; i < array.length; i++){
+            if (array[i] == toFind){
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
+
+    function removeFromArray(address[] storage array, uint index) internal {
+        require(index < array.length);
+        array[index] = array[array.length-1];
+        array.pop();
     }
 
     /* ========== EVENTS ========== */
