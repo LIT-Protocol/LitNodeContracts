2bfccc694d0ee0e4c6d51717998ffc8299f02465
Chris Cassano <chris@litprotocol.com>
2021-12-14 17:27:07 -0500

    changes to how we set validators for the new epoch

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -21,6 +21,7 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
 
     // epoch vars
     uint256 public epoch;
+    uint256 public epochForValidatorsInNextEpoch;
     uint256 public lastEpochBlock;
     uint256 public blocksBetweenEpochs;
 
@@ -33,6 +34,7 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
     address[] public joiners;
     address[] public leavers;
     address[] public validators;
+    address[] public validatorsInNextEpoch;
 
 
     mapping(address => uint32) public ips;
@@ -44,6 +46,29 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         blocksBetweenEpochs = 1;
         tokenRewardPerTokenPerEpoch = (10^ERC20(address(stakingToken)).decimals()) / 20; // 0.05 tokens per token staked meaning a 5% per epoch inflation rate
         minimumStake = 1;
+
+        // for testing
+        validators = [
+            address(0x2e910606fBa7c361Cee433D92ED2Cf21cBBe4EA5),
+            address(0xC027471D03F30669C69ABC6D167835C4987185Ca),
+            address(0x3dbFCf705d31d1B15C49379f2A27F1bbDFCc2544),
+            address(0x4B8bf506c3A84F70F8d4846810b23ea7ba843E7B),
+            address(0x40903E6df2038387eEA2751DE5d88980C8c3Ee9E),
+            address(0x8a328Fee737Ff229c6a0EF73fFfaF745Fa7A9af8),
+            address(0xa2e4dc9bDeB241A6d7DD6b1890508352041295fE),
+            address(0xAA30b2396e0eE819FD3E02aeCd53083828C1daf1),
+            address(0xeB9852914510d8248D367A0E4689Fb5A4e1cF375),
+            address(0x237844451B37231Ea04c25031b86CC943EB0ed14)  
+        ];
+
+        uint32 ip = 2130706433;
+        uint32 startingPort = 7470;
+        for (uint32 i=0; i<validators.length; i++) {
+            ips[validators[i]] = ip;
+            ports[validators[i]] = startingPort + i;
+        }
+
+
     }
 
     /* ========== VIEWS ========== */
@@ -64,34 +89,68 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         return leavers;
     }
 
+
     /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /// Populate validatorsInNextEpoch with the new list of validators.  Adds the joiners and removes the leavers
+    function setValidatorsForNextEpoch() public {
+        require(block.number > lastEpochBlock + blocksBetweenEpochs, "Enough blocks have not elapsed since the last epoch");
+        require(epochForValidatorsInNextEpoch <= epoch, "epochForValidatorsInNextEpoch must be less than or equal the current epoch");
+
+        delete validatorsInNextEpoch;
+
+        bool contained;
+        uint256 index;
+
+        //populate validatorsInNextEpoch with all validators unless the validator is leaving
+        for(uint i = 0; i < validators.length; i++){
+            (contained, index) = arrayContains(leavers, validators[i]);
+            if (!contained){
+                validatorsInNextEpoch.push(validators[i]);
+            }
+        }
+
+        // add the joiners
+        for(uint i = 0; i < joiners.length; i++){
+            validatorsInNextEpoch.push(joiners[i]);
+        }
+
+        delete joiners;
+        delete leavers;
+
+        epochForValidatorsInNextEpoch = epoch + 1;
+    }
 
     /// Advance to the next Epoch.  Rewards validators, adds the joiners, and removes the leavers
     function advanceEpoch() public {
         require(block.number > lastEpochBlock + blocksBetweenEpochs, "Enough blocks have not elapsed since the last epoch");
+        require(epochForValidatorsInNextEpoch > epoch, "epochForValidatorsInNextEpoch must be greater than epoch, indicating that setValidatorsForNextEpoch has already been run.  Please run setValidatorsForNextEpoch and try again.");
 
         // reward the validators
         for(uint i = 0; i < validators.length; i++){
             rewards[validators[i]] += tokenRewardPerTokenPerEpoch * balances[validators[i]];
         }
 
+        // set the validators to the new validator set
+        validators = validatorsInNextEpoch;
 
-        // add the joiners
-        for(uint i = 0; i < joiners.length; i++){
-            validators.push(joiners[i]);
-        }
-        delete joiners;
 
-        // remove the leavers
-        bool contained;
-        uint256 index;
-        for(uint i = 0; i < leavers.length; i++){
-            (contained, index) = arrayContains(validators, leavers[i]);
-            if (contained) {
-                removeFromArray(validators, index);
-            }
-        }
-        delete leavers;
+        // // add the joiners
+        // for(uint i = 0; i < joiners.length; i++){
+        //     validators.push(joiners[i]);
+        // }
+        // delete joiners;
+
+        // // remove the leavers
+        // bool contained;
+        // uint256 index;
+        // for(uint i = 0; i < leavers.length; i++){
+        //     (contained, index) = arrayContains(validators, leavers[i]);
+        //     if (contained) {
+        //         removeFromArray(validators, index);
+        //     }
+        // }
+        // delete leavers;
 
         epoch++;
         lastEpochBlock = block.number;
