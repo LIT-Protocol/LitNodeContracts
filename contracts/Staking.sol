1c065790d3f58ec17f15cff42021de84665ea349
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-03-02 16:19:50 -0500

    events, reordering

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -28,7 +28,6 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         uint endBlock;
     }
     Epoch public epoch;
-    uint256 public epochNumberForValidatorsInNextEpoch;
 
     uint256 public tokenRewardPerTokenPerEpoch;
 
@@ -60,34 +59,6 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         });
         tokenRewardPerTokenPerEpoch = (10^ERC20(address(stakingToken)).decimals()) / 20; // 0.05 tokens per token staked meaning a 5% per epoch inflation rate
         minimumStake = 2;
-
-        // for testing
-        address[10] memory validatorAddresses = [
-            address(0x2e910606fBa7c361Cee433D92ED2Cf21cBBe4EA5),
-            address(0xC027471D03F30669C69ABC6D167835C4987185Ca),
-            address(0x3dbFCf705d31d1B15C49379f2A27F1bbDFCc2544),
-            address(0x4B8bf506c3A84F70F8d4846810b23ea7ba843E7B),
-            address(0x40903E6df2038387eEA2751DE5d88980C8c3Ee9E),
-            address(0x8a328Fee737Ff229c6a0EF73fFfaF745Fa7A9af8),
-            address(0xa2e4dc9bDeB241A6d7DD6b1890508352041295fE),
-            address(0xAA30b2396e0eE819FD3E02aeCd53083828C1daf1),
-            address(0xeB9852914510d8248D367A0E4689Fb5A4e1cF375),
-            address(0x237844451B37231Ea04c25031b86CC943EB0ed14)  
-        ];
-
-        uint32 ip = 2130706433;
-        uint32 startingPort = 7470;
-        for (uint32 i=0; i<validatorAddresses.length; i++) {
-            validators[validatorAddresses[i]] = Validator({
-                ip: ip,
-                port: startingPort + i,
-                nodeAddress: validatorAddresses[i],
-                balance: 0,
-                reward: 0
-            });
-            validatorsInCurrentEpoch.add(validatorAddresses[i]);
-            validatorsInNextEpoch.add(validatorAddresses[i]);
-        }
 
         validatorsForNextEpochLocked = false;
     }
@@ -124,6 +95,7 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
     function lockValidatorsForNextEpoch() public {
         require(block.number >= epoch.endBlock, "Enough blocks have not elapsed since the last epoch");
         validatorsForNextEpochLocked = true;
+        emit ValidatorsForNextEpochLocked(epoch.number);
     }
 
     /// Advance to the next Epoch.  Rewards validators, adds the joiners, and removes the leavers
@@ -170,8 +142,6 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         require(amount >= minimumStake, "Stake must be greater than or equal to minimumStake");
         require(validatorsForNextEpochLocked == false, "Validators for next epoch have already been locked");
 
-        totalStaked = totalStaked.add(amount);
-
         if (!validatorsInNextEpoch.contains(msg.sender)){
             validatorsInNextEpoch.add(msg.sender);
         }
@@ -182,6 +152,9 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         validators[msg.sender].port = port;
         validators[msg.sender].nodeAddress = nodeAddress;
         nodeAddressToStakerAddress[nodeAddress] = msg.sender;
+
+        totalStaked = totalStaked.add(amount);
+
         emit Staked(msg.sender, amount);
     }
 
@@ -250,6 +223,38 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
         minimumStake = newMinimumStake;
     }
 
+    /* ========== TESTING / DEBUG ========== */
+    
+    function setInitialValidatorSet() public onlyOwner{
+        // for testing
+        address[10] memory validatorAddresses = [
+            address(0x2e910606fBa7c361Cee433D92ED2Cf21cBBe4EA5),
+            address(0xC027471D03F30669C69ABC6D167835C4987185Ca),
+            address(0x3dbFCf705d31d1B15C49379f2A27F1bbDFCc2544),
+            address(0x4B8bf506c3A84F70F8d4846810b23ea7ba843E7B),
+            address(0x40903E6df2038387eEA2751DE5d88980C8c3Ee9E),
+            address(0x8a328Fee737Ff229c6a0EF73fFfaF745Fa7A9af8),
+            address(0xa2e4dc9bDeB241A6d7DD6b1890508352041295fE),
+            address(0xAA30b2396e0eE819FD3E02aeCd53083828C1daf1),
+            address(0xeB9852914510d8248D367A0E4689Fb5A4e1cF375),
+            address(0x237844451B37231Ea04c25031b86CC943EB0ed14)  
+        ];
+
+        uint32 ip = 2130706433;
+        uint32 startingPort = 7470;
+        for (uint32 i=0; i<validatorAddresses.length; i++) {
+            validators[validatorAddresses[i]] = Validator({
+                ip: ip,
+                port: startingPort + i,
+                nodeAddress: validatorAddresses[i],
+                balance: 0,
+                reward: 0
+            });
+            validatorsInCurrentEpoch.add(validatorAddresses[i]);
+            validatorsInNextEpoch.add(validatorAddresses[i]);
+        }
+    }
+
     /* ========== EVENTS ========== */
 
     event Staked(address indexed user, uint256 amount);
@@ -257,4 +262,5 @@ contract Staking is ReentrancyGuard, Pausable, Ownable {
     event RewardPaid(address indexed user, uint256 reward);
     event RewardsDurationUpdated(uint256 newDuration);
     event Recovered(address token, uint256 amount);
+    event ValidatorsForNextEpochLocked(uint256 epochNumber);
 }
