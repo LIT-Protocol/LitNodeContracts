6ccc202b16ac49c48b1129841e13de8c30ea32ca
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-05-26 17:15:54 -0700

    routing contract tests pass, including setter test, yay

--- test/PubkeyRouter.js
+++ test/PubkeyRouter.js
@@ -33,9 +33,9 @@ describe("PubkeyRouter", function () {
       it("retrieves empty routing data", async () => {
         const hash = createHash("sha256");
         hash.update(Buffer.from("abcdef", "hex"));
-        const digest = "0x" + hash.digest("hex");
+        const pubkeyHash = "0x" + hash.digest("hex");
         const [keyPart1, keyPart2, keyLength, stakingContract, keyType] =
-          await contract.getRoutingData(digest);
+          await contract.getRoutingData(pubkeyHash);
         expect(keyPart1).equal(
           "0x0000000000000000000000000000000000000000000000000000000000000000"
         );
@@ -50,52 +50,68 @@ describe("PubkeyRouter", function () {
       });
     });
 
-    // context("when routing data is set", async () => {
-    //   let creator;
-    //   let tester;
+    context("when routing data is set", async () => {
+      beforeEach(async () => {
+        contract = contract.connect(deployer);
+      });
 
-    //   beforeEach(async () => {
-    //     [creator, tester, ...signers] = signers;
-    //     contract = contract.connect(creator);
-    //   });
+      it("sets and retrieves routing data", async () => {
+        const fakeBlsPubkey = ethers.utils.randomBytes(48);
 
-    //   beforeEach(async () => (contract = contract.connect(tester)));
+        const hash = createHash("sha256");
+        hash.update(Buffer.from(fakeBlsPubkey));
+        const pubkeyHash = "0x" + hash.digest("hex");
 
-    //   it("retrieves condition and updates it", async () => {
-    //     let [
-    //       valueFromContract,
-    //       chainIdFromContract,
-    //       permanentFromContract,
-    //       creatorFromContract,
-    //     ] = await contract.getCondition(key);
-    //     expect(valueFromContract).equal(value);
-    //     expect(chainIdFromContract).equal(chainId);
-    //     expect(permanentFromContract).equal(permanent);
-    //     expect(creatorFromContract).equal(creator.address);
+        // validate that it's unset
+        const [keyPart1, keyPart2, keyLength, stakingContract, keyType] =
+          await contract.getRoutingData(pubkeyHash);
+        expect(keyPart1).equal(
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
+        expect(keyPart2).equal(
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
+        expect(keyLength).equal(0);
+        expect(stakingContract).equal(
+          "0x0000000000000000000000000000000000000000"
+        );
+        expect(keyType).equal(0);
 
-    //     const newValue = 0x8765;
-    //     const newChainId = 2;
+        // set routing data
+        const keyPart1Bytes = ethers.utils.hexDataSlice(fakeBlsPubkey, 0, 32);
+        const keyPart2Bytes = ethers.utils.hexZeroPad(
+          ethers.utils.hexDataSlice(fakeBlsPubkey, 32),
+          32
+        );
 
-    //     // attempt to update it with the wrong address.  it should revert.
-    //     expect(
-    //       contract.storeCondition(key, newValue, newChainId, permanent)
-    //     ).revertedWith("Only the condition creator can update it");
+        const stakingContractAddress =
+          "0x6cB3Cd5064692ac8e3368A1d6C29b36aE1143dF7"; // a random address
+        const keyLengthInput = 48;
+        const keyTypeInput = 1;
 
-    //     // update with the correct address
-    //     contract = contract.connect(creator);
-    //     await contract.storeCondition(key, newValue, newChainId, permanent);
+        await contract.setRoutingData(
+          pubkeyHash,
+          keyPart1Bytes,
+          keyPart2Bytes,
+          keyLengthInput,
+          stakingContractAddress,
+          keyTypeInput
+        );
 
-    //     [
-    //       valueFromContract,
-    //       chainIdFromContract,
-    //       permanentFromContract,
-    //       creatorFromContract,
-    //     ] = await contract.getCondition(key);
-    //     expect(valueFromContract).equal(newValue);
-    //     expect(chainIdFromContract).equal(newChainId);
-    //     expect(permanentFromContract).equal(permanent);
-    //     expect(creatorFromContract).equal(creator.address);
-    //   });
-    // });
+        // validate that it was set
+        const [
+          keyPart1After,
+          keyPart2After,
+          keyLengthAfter,
+          stakingContractAfter,
+          keyTypeAfter,
+        ] = await contract.getRoutingData(pubkeyHash);
+        expect(keyPart1After).equal(keyPart1Bytes);
+        expect(keyPart2After).equal(keyPart2Bytes);
+        expect(keyLengthAfter).equal(keyLengthInput);
+        expect(stakingContractAfter).equal(stakingContractAddress);
+        expect(keyTypeAfter).equal(keyTypeInput);
+      });
+    });
   });
 });
