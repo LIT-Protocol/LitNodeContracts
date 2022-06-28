9d0a25109021a0e552db610392c8dae83530f0bc
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-06-28 00:59:11 -0400

    comment out ethernal becauxe it's making the tests not run for some unknown reason

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
-require('hardhat-ethernal'); // required for ethernal - removing this will only break deploy scripts that are linked to ethernal .  
-  // Commenting out lines in these deploy scripts doesn't break the deploy, only the synchronization with Ethernal - ie, safe to do. 
-  // Tests & regular deploys continue to work normally.
-  // Ethernal is a web based block explorer that syncs from any EVM chain - easy way to "view" hardhat data, and execute contracts!
-  // https://www.tryethernal.com
-
+// require('hardhat-ethernal'); // required for ethernal - removing this will only break deploy scripts that are linked to ethernal .
+// Commenting out lines in these deploy scripts doesn't break the deploy, only the synchronization with Ethernal - ie, safe to do.
+// Tests & regular deploys continue to work normally.
+// Ethernal is a web based block explorer that syncs from any EVM chain - easy way to "view" hardhat data, and execute contracts!
+// https://www.tryethernal.com
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -25,4 +24,3 @@ task("accounts", "Prints the list of accounts", async () => {
 module.exports = {
   solidity: "0.8.4",
 };
-
