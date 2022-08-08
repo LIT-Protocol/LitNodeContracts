031d4535c7d02080db5114f7ec928bfe34bdceec
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-08-08 10:46:16 -0600

    free mint signer should work

---
+++ scripts/newWallet.js
@@ -0,0 +1,10 @@
+const { ethers } = require("ethers");
+
+const wallet = ethers.Wallet.createRandom();
+let walletJson = {
+  address: wallet.address,
+  privateKey: wallet.privateKey,
+  publicKey: wallet.publicKey,
+  mnemonic: wallet.mnemonic.phrase,
+};
+console.log("walletJson", JSON.stringify(walletJson));
