c09d5014dea59e6cb4033d3b7bf9c0845127f8f2
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-10-11 11:59:55 -0500

    add gas tracking

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,8 @@ require("@nomiclabs/hardhat-waffle");
 
 require("@nomiclabs/hardhat-etherscan");
 
+require("hardhat-gas-reporter");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
