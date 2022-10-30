08e0f486b31bb38863b83e00fae3b778bc5d7dc8
Howard Tam <howard.tam.95@gmail.com>
2022-10-30 12:28:44 +0800

    make condition storing MEV-resistant, and introduce signer for trusted condition storing

--- contracts/AccessControlConditions.sol
+++ contracts/AccessControlConditions.sol
@@ -1,14 +1,19 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 import "hardhat/console.sol";
 
-contract AccessControlConditions is ReentrancyGuard {
+contract AccessControlConditions is
+    Ownable,
+    ReentrancyGuard
+{
     /* ========== STRUCTS ========== */
     struct StoredCondition {
         uint256 value;
+        uint256 securityHash;
         uint256 chainId;
         bool permanent;
         address creator;
@@ -17,9 +22,12 @@ contract AccessControlConditions is ReentrancyGuard {
     /* ========== STATE VARIABLES ========== */
 
     mapping(uint256 => StoredCondition) public storedConditions;
+    address public signer;
 
     /* ========== CONSTRUCTOR ========== */
-    constructor() {}
+    constructor() {
+        signer = msg.sender;
+    }
 
     /* ========== VIEWS ========== */
 
@@ -36,14 +44,44 @@ contract AccessControlConditions is ReentrancyGuard {
     function storeCondition(
         uint256 key,
         uint256 value,
+        uint256 securityHash,
         uint256 chainId,
         bool permanent
     ) external nonReentrant {
+        _storeCondition(key, value, securityHash, chainId, permanent, msg.sender);
+    }
+
+    function storeConditionWithSigner(
+        uint256 key,
+        uint256 value,
+        uint256 securityHash,
+        uint256 chainId,
+        bool permanent,
+        address creatorAddress
+    ) external nonReentrant {
+        require(msg.sender == signer, "Only signer can call storeConditionsWithSigner.");
+        _storeCondition(key, value, securityHash, chainId, permanent, creatorAddress);
+    }
+
+    function setSigner(address newSigner) public onlyOwner {
+        signer = newSigner;
+    }
+
+    /* ========== PRIVATE FUNCTIONS ========== */
+
+    function _storeCondition(
+        uint256 key,
+        uint256 value,
+        uint256 securityHash,
+        uint256 chainId,
+        bool permanent,
+        address creatorAddress
+    ) private {
         require(key != 0, "Key must not be zero");
         if (storedConditions[key].creator != address(0)) {
             // this is an update
             require(
-                storedConditions[key].creator == msg.sender,
+                storedConditions[key].creator == creatorAddress,
                 "Only the condition creator can update it"
             );
             require(
@@ -53,12 +91,13 @@ contract AccessControlConditions is ReentrancyGuard {
         }
         storedConditions[key] = StoredCondition(
             value,
+            securityHash,
             chainId,
             permanent,
-            msg.sender
+            creatorAddress
         );
 
-        emit ConditionStored(key, value, chainId, permanent, msg.sender);
+        emit ConditionStored(key, value, chainId, permanent, creatorAddress);
     }
 
     /* ========== EVENTS ========== */
