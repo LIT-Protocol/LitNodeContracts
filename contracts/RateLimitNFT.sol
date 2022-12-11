1839e82116762c21e31f49601582c28aef18648f
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-12-10 19:10:38 -0500

    added events to rate limit nft

--- contracts/RateLimitNFT.sol
+++ contracts/RateLimitNFT.sol
@@ -83,13 +83,9 @@ contract RateLimitNFT is
         );
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
         return
             interfaceId == type(IERC721Enumerable).interfaceId ||
             interfaceId == type(IERC721Metadata).interfaceId ||
@@ -104,11 +100,10 @@ contract RateLimitNFT is
         ERC721Enumerable._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function calculateCost(uint256 requestsPerMillisecond, uint256 expiresAt)
-        public
-        view
-        returns (uint256)
-    {
+    function calculateCost(
+        uint256 requestsPerMillisecond,
+        uint256 expiresAt
+    ) public view returns (uint256) {
         require(
             expiresAt > block.timestamp,
             "The expiresAt must be in the future"
@@ -125,11 +120,10 @@ contract RateLimitNFT is
         return cost;
     }
 
-    function calculateRequestsPerSecond(uint256 payingAmount, uint256 expiresAt)
-        public
-        view
-        returns (uint256)
-    {
+    function calculateRequestsPerSecond(
+        uint256 payingAmount,
+        uint256 expiresAt
+    ) public view returns (uint256) {
         require(
             expiresAt > block.timestamp,
             "The expiresAt must be in the future"
@@ -145,12 +139,9 @@ contract RateLimitNFT is
         return requestsPerSecond;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         string
             memory svgData = "<svg xmlns='http://www.w3.org/2000/svg' width='1080' height='1080' fill='none' xmlns:v='https://vecta.io/nano'><path d='M363.076 392.227s-.977 18.524-36.874 78.947c-41.576 70.018-45.481 151.978-3.017 220.4 89.521 144.245 332.481 141.52 422.556.089 34.832-54.707 44.816-117.479 32.924-181.248 0 0-28.819-133.144-127.237-217.099 1.553 1.308 5.369 19.122 6.101 26.722 2.241 23.354.045 47.838-7.787 70.062-5.746 16.33-13.711 30.467-27.178 41.368 0-3.811-.954-10.635-.976-12.918-.644-46.508-18.659-89.582-48.011-125.743-25.647-31.552-60.812-53.089-97.84-68.932.931 3.191 2.662 16.419 2.906 19.033 1.908 21.958 2.263 52.713-.621 74.649s-7.832 33.878-14.554 54.441c-10.184 31.175-24.05 54.285-41.621 82.004-3.24 5.096-12.913 19.078-18.082 26.146 0 0-8.897-56.191-40.667-87.921h-.022z' fill='#000'/><path d='M562.5 27.28l410.279 236.874c13.923 8.039 22.5 22.895 22.5 38.971v473.75c0 16.076-8.577 30.932-22.5 38.971L562.5 1052.72c-13.923 8.04-31.077 8.04-45 0L107.221 815.846c-13.923-8.039-22.5-22.895-22.5-38.971v-473.75a45 45 0 0 1 22.5-38.971L517.5 27.28a45 45 0 0 1 45 0z' stroke='#000' stroke-width='24.75'/></svg>";
 
@@ -237,34 +228,62 @@ contract RateLimitNFT is
         uint256 newAdditionalRequestsPerMillisecondCost
     ) public onlyOwner {
         additionalRequestsPerMillisecondCost = newAdditionalRequestsPerMillisecondCost;
+        emit AdditionalRequestsPerSecondCostSet(
+            newAdditionalRequestsPerMillisecondCost
+        );
     }
 
     function setFreeMintSigner(address newFreeMintSigner) public onlyOwner {
         freeMintSigner = newFreeMintSigner;
+        emit FreeMintSignerSet(newFreeMintSigner);
     }
 
     function withdraw() public onlyOwner nonReentrant {
-        (bool sent, ) = payable(msg.sender).call{value: address(this).balance}(
-            ""
-        );
+        uint withdrawAmount = address(this).balance;
+        (bool sent, ) = payable(msg.sender).call{value: withdrawAmount}("");
         require(sent);
+        emit Withdrew(withdrawAmount);
     }
 
     function setRateLimitWindowMilliseconds(
         uint256 newRateLimitWindowMilliseconds
     ) public onlyOwner {
         defaultRateLimitWindowMilliseconds = newRateLimitWindowMilliseconds;
+        emit RateLimitWindowMillisecondsSet(newRateLimitWindowMilliseconds);
     }
 
     function setRLIHolderRateLimitWindowMilliseconds(
         uint256 newRLIHolderRateLimitWindowMilliseconds
     ) public onlyOwner {
         RLIHolderRateLimitWindowMilliseconds = newRLIHolderRateLimitWindowMilliseconds;
+        emit RLIHolderRateLimitWindowMillisecondsSet(
+            newRLIHolderRateLimitWindowMilliseconds
+        );
     }
 
     function setFreeRequestsPerRateLimitWindow(
         uint256 newFreeRequestsPerRateLimitWindow
     ) public onlyOwner {
         freeRequestsPerRateLimitWindow = newFreeRequestsPerRateLimitWindow;
+        emit FreeRequestsPerRateLimitWindowSet(
+            newFreeRequestsPerRateLimitWindow
+        );
     }
+
+    /* ========== EVENTS ========== */
+
+    event AdditionalRequestsPerSecondCostSet(
+        uint256 newAdditionalRequestsPerMillisecondCost
+    );
+    event FreeMintSignerSet(address indexed newFreeMintSigner);
+    event Withdrew(uint amount);
+    event RateLimitWindowMillisecondsSet(
+        uint256 newRateLimitWindowMilliseconds
+    );
+    event RLIHolderRateLimitWindowMillisecondsSet(
+        uint256 newRLIHolderRateLimitWindowMilliseconds
+    );
+    event FreeRequestsPerRateLimitWindowSet(
+        uint256 newFreeRequestsPerRateLimitWindow
+    );
 }
