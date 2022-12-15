234450a0c0e250e6c1085f76c6ccc62b02b5c940
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-12-15 12:05:25 -0500

    added allowlist contract

---
+++ contracts/Allowlist.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import "hardhat/console.sol";
+
+contract Allowlist is Ownable, ReentrancyGuard {
+    /* ========== STATE VARIABLES ========== */
+
+    mapping(bytes32 => bool) public allowedItems;
+
+    /* ========== CONSTRUCTOR ========== */
+    constructor() {}
+
+    /* ========== VIEWS ========== */
+
+    function isAllowed(bytes32 key) external view returns (bool) {
+        return allowedItems[key];
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function setAllowed(bytes32 key) external nonReentrant onlyOwner {
+        allowedItems[key] = true;
+        emit ItemAllowed(key);
+    }
+
+    function setNotAllowed(bytes32 key) external nonReentrant onlyOwner {
+        allowedItems[key] = false;
+        emit ItemNotAllowed(key);
+    }
+
+    /* ========== EVENTS ========== */
+
+    event ItemAllowed(bytes32 indexed key);
+    event ItemNotAllowed(bytes32 indexed key);
+}
---
+++ test/Allowlist.js
@@ -0,0 +1,87 @@
+const { expect } = require("chai");
+// const hre = require("hardhat");
+// const { ethers } = hre;
+// const chainName = hre.network.name;
+
+// console.log("chainName", chainName);
+
+describe("Allowlist", function () {
+  let signers;
+  let contract;
+
+  let ContractFactory;
+
+  before(async () => {
+    ContractFactory = await ethers.getContractFactory("Allowlist");
+  });
+
+  beforeEach(async () => {
+    signers = await ethers.getSigners();
+  });
+
+  beforeEach(async () => {
+    contract = await ContractFactory.deploy();
+  });
+
+  describe("Test the Allowlist", async () => {
+    context("unallowed by default", async () => {
+      let deployer;
+      let tester;
+
+      beforeEach(async () => ([deployer, tester, ...signers] = signers));
+
+      beforeEach(async () => (contract = contract.connect(tester)));
+
+      it("is unallowed", async () => {
+        const allowed = await contract.isAllowed(
+          ethers.utils.keccak256("0x123456")
+        );
+        expect(allowed).equal(false);
+      });
+    });
+
+    context("when the owner sets things", async () => {
+      let deployer;
+      let tester;
+
+      const key = "0x123456789f";
+
+      beforeEach(async () => {
+        [deployer, tester, ...signers] = signers;
+      });
+
+      beforeEach(async () => (contract = contract.connect(tester)));
+
+      it("can allow and unallow things", async () => {
+        // unallowed by default
+        let allowed = await contract.isAllowed(ethers.utils.keccak256(key));
+        expect(allowed).equal(false);
+
+        // attempt to allow it with the wrong address.  it should revert.
+        expect(contract.setAllowed(ethers.utils.keccak256(key))).revertedWith(
+          "Ownable: caller is not the owner"
+        );
+
+        // connect the owner and try allowing
+        contract = contract.connect(deployer);
+        await contract.setAllowed(ethers.utils.keccak256(key));
+
+        allowed = await contract.isAllowed(ethers.utils.keccak256(key));
+        expect(allowed).equal(true);
+
+        contract = contract.connect(tester);
+        // attempt to unallow it with the wrong address.  it should revert.
+        expect(
+          contract.setNotAllowed(ethers.utils.keccak256(key))
+        ).revertedWith("Ownable: caller is not the owner");
+
+        // connect the owner and try unallowing
+        contract = contract.connect(deployer);
+        await contract.setNotAllowed(ethers.utils.keccak256(key));
+
+        allowed = await contract.isAllowed(ethers.utils.keccak256(key));
+        expect(allowed).equal(false);
+      });
+    });
+  });
+});
