e553266ffb9e6b1210c30828876aaa9c60678768
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-03-21 00:32:02 -0400

    add rollup chains

--- ipfsIdToAllowlistHash.js
+++ ipfsIdToAllowlistHash.js
@@ -1,6 +1,8 @@
 const { ethers } = require("ethers");
 const ipfsId = "QmRwN9GKHvCn4Vk7biqtr6adjXMs7PzzYPCzNCRjPFiDjm";
 
-const converted = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(ipfsId));
+const converted = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes("LIT_ACTION_" + ipfsId)
+);
 
 console.log("as bytes: ", converted);
