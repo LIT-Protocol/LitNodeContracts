125bf72a56fc48f552a3166f5b7b85052afb040b
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-03-23 18:08:59 -0400

    working well on rollup

--- deployEverything.sh
+++ deployEverything.sh
@@ -3,7 +3,7 @@
 set -e
 . "./deployInit.sh"
 
-
+echo "Deploy init completed"
 
 npx hardhat run --network "${NETWORK}" scripts/deploy_everything.js && npx hardhat run --network "${NETWORK}" scripts/fund_and_stake_nodes.js
 
